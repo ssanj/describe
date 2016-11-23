d78061876d053ddc034e21eebfecdf14576760a0
sanjiv sahayam <sanjsmailbox@gmail.com>
2016-11-23 23:30:24 +1100

    add method filtration methods.

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 A Scala reflection utility to quickly list details of a type.
 
+## Features ##
+
+Should provide quick access to the following for a type:
+
+1. methods
+  1. Allow searching by name, param types, flags and return types
+  1. Include implicit conversion methods?
+1. constructors
+  1. Allow searching by param types
+1. classes
+1. baseclasses
+1. subclasses
+1. implicits (across the companion as well)
+1. Final type
+1. Alias (if any)
--- src/main/scala/net/ssanj/describe/api/Members.scala
+++ src/main/scala/net/ssanj/describe/api/Members.scala
@@ -74,13 +74,49 @@ import scala.reflect.runtime.universe._
       else Option(ClassInfo(ts.asClass))
     }
 
+    lazy val finalResultType = ttType.finalResultType
+
+    lazy val resultType = ttType.resultType
+
+    def methodsByParam[T: TypeTag]: Seq[MethodInfo] =
+      methods.filter(_.paramLists.exists(_.exists(_.paramType =:= typeOf[T])))
+
+    def methodsByParam2[T: TypeTag, U: TypeTag]: Seq[MethodInfo] =
+      methods.filter(_.paramLists.exists { l =>
+        val result =
+          l.foldLeft((false, false)){(acc, p) => acc match {
+            case (false, false) => (p.paramType =:= typeOf[T], p.paramType =:= typeOf[U])
+            case (true, false)  => (acc._1, p.paramType =:= typeOf[U])
+            case (false, true)  => (p.paramType =:= typeOf[T], acc._2)
+            case (true, true)   => acc
+          }}
+        result._1 && result._2
+      })
+
     def methodsByName(reg: String): Seq[MethodInfo] = methods.filter(_.methodName.matches(reg))
+
+    def methodsBy(f: MethodInfo => Boolean): Seq[MethodInfo] = methods.filter(f)
+
+    def methodsReturning[T: TypeTag]: Seq[MethodInfo] = methods.filter(_.returnType.finalResultType =:= typeOf[T])
+
+    def methodsReturningTypeParams: Seq[MethodInfo] =
+      methods.filter(_.returnType.finalResultType.typeSymbol.isParameter)
 
     lazy val constructors: Seq[MethodInfo] = methods.filter(_.isConstructor)
 
     lazy val implicitMethods: Seq[MethodInfo] = methods.filter(_.isImplicit)
 
     lazy val implicitClasses: Seq[ClassInfo] = classes.filter(_.isImplicit)
+
+    lazy val allImplicitMethods: Seq[MethodInfo] = {
+      implicitMethods ++
+      companion.toSeq.flatMap(_.implicitMethods)
+    }
+
+    lazy val allImplicitClasses: Seq[ClassInfo] = {
+      implicitClasses ++
+      companion.toSeq.flatMap(_.implicitClasses)
+    }
 
     lazy val companion: Option[MemberInfo] = {
       val comp = ttType.dealias.etaExpand.companion
@@ -110,6 +146,8 @@ import scala.reflect.runtime.universe._
 trait Members {
 
   def info[T: TypeTag]: MemberInfo = MemberInfo(typeOf[T])
+
+  def info[T: TypeTag](value: T): MemberInfo = MemberInfo(typeOf[T])
 
   // private def filterMembers[T: TypeTag, U <: Symbol](filter: PartialFunction[Symbol, U]): Seq[U] = {
   //   members[T].collect(filter).toList.toSeq
