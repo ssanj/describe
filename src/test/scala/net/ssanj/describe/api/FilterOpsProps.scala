dcbff952ff9a038bfcd3921df4e9a9fe4e22229e
sanjiv sahayam <sanjsmailbox@gmail.com>
2016-12-27 01:04:09 +1100

    add tests for withoutAnyRef

--- src/test/scala/net/ssanj/describe/api/FilterOpsProps.scala
+++ src/test/scala/net/ssanj/describe/api/FilterOpsProps.scala
@@ -2,57 +2,29 @@ package net.ssanj.describe
 package api
 
 import org.scalacheck.Properties
-import org.scalacheck.{Prop, Gen}
-import org.scalacheck.Prop.BooleanOperators
-import Show.AnyContext
+import org.scalacheck.Prop
+
+import PropUtils._
 
 object FilterOpsProps extends Properties("FilterOps") {
 
-  sealed trait MethodName
-
-  implicit val methodNameShow = Show.create[MethodName, MethodInfo](_.name)
-
-  private def am[C, T](desc: String)(expected: T, actual: T)(implicit S: Show[C, T]): Prop = {
-    (expected == actual) :| (
-      s"$desc" +
-      s"${System.lineSeparator}expected:${System.lineSeparator}${S.show(expected)}" +
-      s"${System.lineSeparator}actual:${System.lineSeparator}${S.show(actual)}"
-    )
-  }
-
-  private def genMemberInfo: Gen[MemberInfo] =
-    Gen.oneOf(
-      getPackage[Option[_]].toSeq.
-        map( p =>
-          p.classes.collect {
-            case c if !c.isAbstract &&
-                      !c.isJava &&
-                      !c.asType.methods.isEmpty => c.asType
-          }
-        ).flatten
-    )
-
-  property("withoutAny") =
+  private def assertWithout(
+    withMethodsRemoved: Seq[MethodInfo] => Seq[MethodInfo],
+    methodsToRemove: Seq[MethodInfo],
+    id: String): Prop = {
     Prop.forAll(genMemberInfo) { mi: MemberInfo =>
       Prop.collect(mi.fullName){
         val miMethods = mi.methods
-        val miMethodsWithoutAny = miMethods.withoutAny
-        val expected = miMethods.filterNot(anyMethods.contains)
+        val miMethodsRemoved = withMethodsRemoved(miMethods).map(MethodNameShow)
+        val expectedMethods = miMethods.filterNot(methodsToRemove.contains).map(MethodNameShow)
 
-        am("methods should not be empty")(true, miMethods.nonEmpty)
-        am[MethodName, Seq[MethodInfo]]("methods should not contain Any methods.")(expected, miMethodsWithoutAny)
-      }
-    }
-
-  property("withoutAnyRef") =
-    Prop.forAll(genMemberInfo) { mi: MemberInfo =>
-      Prop.collect(mi.fullName){
-        val miMethods = mi.methods
-        val miMethodsWithoutAnyRef = miMethods.withoutAnyRef
-        val expected = miMethods.filterNot(anyRefMethods.contains)
-
-        am[AnyContext, Boolean]("methods should not be empty")(true, miMethods.nonEmpty)
-        am[MethodName, Seq[MethodInfo]]("methods should not contain AnyRef methods.")(expected, miMethodsWithoutAnyRef)
+        propAssert("Methods should not be empty.")(Seq(true), Seq(miMethods.nonEmpty)) &&
+        propAssert(s"Methods should not contain $id methods.")(expectedMethods, miMethodsRemoved)
       }
     }
   }
+
+  property("withoutAny") = assertWithout(_.withoutAny, anyMethods, "Any")
+
+  property("withoutAnyRef") = assertWithout(_.withoutAnyRef, anyRefMethods, "AnyRef")
+}
--- src/test/scala/net/ssanj/describe/api/common.scala
+++ src/test/scala/net/ssanj/describe/api/common.scala
@@ -1,22 +1,58 @@
 package net.ssanj.describe
 package api
 
-trait Show[C, T] {
+trait Show[T] {
   def show(value: T): String
 }
 
-object Show {
-  def apply[C, T](implicit showct: Show[C, T]): Show[C, T] = showct
+trait LowPriorityShowImplicits {
+  implicit def toStringToShow[T]: Show[T] = new Show[T] {
+    override def show(value: T): String = value.toString
+  }
+}
 
-  def create[C, T](print: T => String): Show[C, T] = new Show[C, T] {
+final case class MethodNameShow(value: MethodInfo)
+
+object Show extends LowPriorityShowImplicits {
+  def apply[T](implicit S: Show[T]): Show[T] = S
+
+  def create[T](print: T => String): Show[T] = new Show[T] {
     def show(value: T): String = print(value)
   }
 
-  implicit def fromSeq[C, T](implicit showct: Show[C, T]): Show[C, Seq[T]] = {
-    Show.create[C, Seq[T]](_.map(v => s"\t${showct.show(v)}").mkString("\n"))
+  implicit def fromSeq[T](implicit S: Show[T]): Show[Seq[T]] = {
+    Show.create[Seq[T]](_.map(v => s"\t${S.show(v)}").mkString("\n"))
   }
 
-  sealed trait AnyContext
+  implicit val methodNameShow = Show.create[MethodNameShow](_.value.name)
+}
 
-  implicit val booleanShow = Show.create[AnyContext, Boolean](_.toString)
+object PropUtils {
+  import org.scalacheck.{Gen, Prop}
+  import org.scalacheck.Prop.BooleanOperators
+
+  def genMemberInfo: Gen[MemberInfo] =
+    Gen.oneOf(
+      getPackage[Option[_]].toSeq.
+        map( p =>
+          p.classes.collect {
+            case c if !c.isAbstract &&
+                      !c.isJava &&
+                      !c.asType.methods.isEmpty => c.asType
+          }
+        ).flatten
+    )
+
+  def propAssert[T: Show](desc: String)(expected: Seq[T], actual: Seq[T]): Prop = {
+    val S = implicitly[Show[Seq[T]]]
+    (expected == actual) :| (
+      s"$desc" +
+      s"${System.lineSeparator}In expected but not actual:" +
+        s"${System.lineSeparator}${S.show(expected.filterNot(actual.contains))}" +
+      s"${System.lineSeparator}In actual but not expected:" +
+        s"${System.lineSeparator}${S.show(actual.filterNot(expected.contains))}" +
+      s"${System.lineSeparator}In actual and expected:" +
+        s"${System.lineSeparator}${S.show(actual.filter(expected.contains))}"
+    )
+  }
 }
