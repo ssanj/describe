83887d5b88069e70865d68dbdbc24ae986cb96ea
sanjiv sahayam <sanjsmailbox@gmail.com>
2017-06-27 22:17:21 +1000

    add more base case tests

--- src/test/scala/net/ssanj/describe/api/SampleClasses.scala
+++ src/test/scala/net/ssanj/describe/api/SampleClasses.scala
@@ -3,12 +3,12 @@ package net.ssanj.describe.api
 class NoCompanionObject
 object NoCompanionClass
 
-class Animal {
-  var happiness = 0
+class Animal (base: Int, name: String) {
+  var happiness = base
 
   val roar: String = "Growl"
 
-  val getName: String = "Snappy"
+  val getName: String = name
 
   def pet(pats: Int): Int = {
     happiness += pats
